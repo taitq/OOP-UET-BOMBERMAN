e735a000d05b9b28459058e69869b60b5ece79c1
tunangoo <ngovantuan02092003@gmail.com>
2022-09-25 17:12:48 +0700

    update number of bomb and handle input for bomber

--- src/main/java/GameEntites/Bomber.java
+++ src/main/java/GameEntites/Bomber.java
@@ -1,9 +1,35 @@
 package GameEntites;
 
+import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.input.KeyCode;
+
+import java.awt.event.KeyListener;
 
 public class Bomber extends MoveAnimation {
-    public Bomber(int x, int y, Image image, int speed) {
+    private int numberOfBomb;
+
+    public Bomber(int x, int y, Image image, int speed, int numberOfBomb) {
         super(x, y, image, speed);
+        this.numberOfBomb = Math.max(1, numberOfBomb);
+    }
+
+    /**
+     * nhận sự kiện di chuyển từ bàn phím cho bomber.
+     */
+    public void handleInput(Scene scene) {
+        scene.setOnKeyPressed(event -> {
+            if (event.getCode() == KeyCode.UP) {
+                moveUp();
+            } else if (event.getCode() == KeyCode.DOWN) {
+                moveDown();
+            } else if (event.getCode() == KeyCode.LEFT) {
+                moveLeft();
+            } else if (event.getCode() == KeyCode.RIGHT) {
+                moveRight();
+            } else if (event.getCode() == KeyCode.SPACE) {
+                //dat bomb
+            }
+        });
     }
 }
