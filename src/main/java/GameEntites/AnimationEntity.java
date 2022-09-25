69d27f31ca4097080d79186961bc6fff18a01dd2
taitq <trqt2003@gmail.com>
2022-09-25 23:31:44 +0700

    delete speed in Unmove class

--- src/main/java/GameEntites/AnimationEntity.java
+++ src/main/java/GameEntites/AnimationEntity.java
@@ -6,8 +6,7 @@ import javafx.scene.image.Image;
  * Đối tượng mà có thay đổi về vị trí hay hiệu ứng,.. trong chương trình.
  */
 public abstract class AnimationEntity extends Entity {
-    protected int speed;
-    public AnimationEntity(int x, int y, Image image, int speed) {
+    public AnimationEntity(int x, int y, Image image) {
         super(x, y, image);
     }
 
